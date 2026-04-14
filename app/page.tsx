781983635049a6550f28fca26730e83ed8c571c9
Mantry <vg1317178267@gmail.com>
2026-04-14 10:41:45 -0400

    Fetch recipe data from API

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [recipes, setRecipes] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.npoint.io/6c48278e70bb1329ec40")
+      .then((res) => res.json())
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <main style={{ padding: "30px", fontFamily: "Arial" }}>
       <h1>Recipe Table</h1>
-      <p>Recipes loaded from API will appear below.</p>
 
-      {/* table will go here later */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>Loaded {recipes.length} recipes</p>
+      )}
     </main>
   );
 }
